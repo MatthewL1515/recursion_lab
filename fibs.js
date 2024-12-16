d1f9b8e65db90228d42cc9cdbecaa9f9f1efeabf
Matthew Lee <Matthew_Lee@avenues-ny.org>
2024-12-15 23:01:05 -0500

    Update fibs.js

--- fibs.js
+++ fibs.js
@@ -1,1 +1,36 @@
+let fibs = [0,1]
+let n = 1
 
+function setup() {
+  createCanvas(400, 400)
+  noLoop()
+  textAlign(CENTER, CENTER)
+}
+
+function draw() {
+  background(220)
+  // text(`${n}th fibonacci number: ${fibs[n]}` ,width/2,height/2)
+  text(`${n}th fibonacci number: ${fib(n)}`,width/2,height/2)
+  
+}
+
+function keyPressed() {
+  if( key === " " ) {
+    n += 1    
+    redraw()
+  }
+}
+
+
+// Going to edit here only
+function fib(n) {
+  if( n < 1 ) { // error/base case
+    return 0
+  } 
+  
+  for (let i = fibs.length; i <= n; i++) { // found .length to be # of values in array
+    fibs[i] = fibs[i - 1] + fibs[i - 2] // Adding prev. 2 numbers ONLY
+  }
+    
+  return fibs[n] // Store calculated numbers in array
+}
